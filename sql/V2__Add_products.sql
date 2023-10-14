046e97c0db20c2ccc7f91912a3f58ce57fadbe6a
ambarisha <ambarisha.malladi@gmail.com>
2023-10-14 13:51:30 +0530

    Second commit

---
+++ sql/V2__Add_products.sql
@@ -0,0 +1,4 @@
+INSERT INTO product (id, product_name) VALUES (1, 'Chair');
+INSERT INTO product (id, product_name) VALUES (2, 'Table');
+INSERT INTO product (id, product_name) VALUES (3, 'Couch');
+INSERT INTO product (id, product_name) VALUES (4, 'Dining Table');
